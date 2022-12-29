d64877fe59e08d58627a0cc8e820f8351f7d7c7e
Cole Dreyer <45116482+Cole-Dreyer@users.noreply.github.com>
2022-12-28 22:45:30 -0500

    Add files via upload

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,20 @@ const path = require("path");
 
 require("dotenv").config();
 
+// create variables to hold URLs for both databases listed in .env
+// create variable to hold the NODE_ENV status.
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
+
+// create variable to hold URL depending on if migration is run
+// with NODE_ENV as development or as production.
+const DATABASE_URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
