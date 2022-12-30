93b667c7ea27ce485f12bc4bc9bc96f80f68f5e1
Cole Dreyer <45116482+Cole-Dreyer@users.noreply.github.com>
2022-12-29 22:14:47 -0500

    Add files via upload

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,9 @@ const path = require("path");
 
 require("dotenv").config();
 
-// create variables to hold URLs for both databases listed in .env
-// create variable to hold the NODE_ENV status.
 const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL = "postgres://skaykxav:5x6RWn0vHkF3GYZ-JQ4i60HXOtkPxaO9@berry.db.elephantsql.com/skaykxav",
-  PRODUCTION_DATABASE_URL = "postgres://skaykxav:5x6RWn0vHkF3GYZ-JQ4i60HXOtkPxaO9@berry.db.elephantsql.com/skaykxav",
+  DATABASE_URL = "postgres://glijgiqu:Qkdtn3YGkfa-bd-tS814AxPVHrP9i4TB@berry.db.elephantsql.com/glijgiqu",
 } = process.env;
-
-// create variable to hold URL depending on if migration is run
-// with NODE_ENV as development or as production.
-const DATABASE_URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
